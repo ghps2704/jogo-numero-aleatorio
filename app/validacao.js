9b3ce34b7462489e97cbbfee1985f1ae927dd29b
Guilherme Henrique <100978223+ghps2704@users.noreply.github.com>
2023-09-20 16:31:33 -0300

    Add files via upload
    jogo de reconhecimento de voz feito com javascript

---
+++ app/validacao.js
@@ -0,0 +1,45 @@
+function verificaValorValido(chute) {
+    const numero = +chute
+
+    if (chuteForInvalido(numero)){
+        elementoChute.innerHTML += '<div>Valor inválido</div>'
+        return
+
+    }
+
+    if (numeroNaoPermitido(numero)){
+        elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
+        return
+    }
+
+    if (numero === numeroSecreto){
+        document.body.innerHTML = `
+            <h2>Você acertou!</h2>
+            <h3>O número secreto era ${numeroSecreto}</h3>
+
+            <button id="jogar-novamente" class="btn-jogar">Jogar Novamente</button>
+        `
+    } else if (numero > numeroSecreto){
+        elementoChute.innerHTML += `
+            <div>O número secreto é menor <i class="fa-solid fa-arrow-turn-down"></i></div>
+        `
+    } else {
+        elementoChute.innerHTML += `
+            <div>O número secreto é maior <i class="fa-solid fa-arrow-turn-up"></i></div>
+        `
+    }
+}
+
+function chuteForInvalido(numero) {
+    return Number.isNaN(numero)
+}
+
+function numeroNaoPermitido(numero){
+    return numero > maiorValor || numero < menorValor
+}
+
+document.body.addEventListener('click', e => {
+    if (e.target.id == 'jogar-novamente'){
+        window.location.reload()
+    }
+})
